2893433b3bc478af21ce0bb40120e7733377fb6b
haseary <hase.ray.business@gmail.com>
2023-10-16 21:19:18 +0900

    add published_at to Video#show2

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -5,13 +5,13 @@ class VideosController < ApplicationController
     @videos = @videos.where("title LIKE ?", "%#{word}%")
     @videos = @videos.page(20).page(params[:page])
     @videos = @videos.map {|video| {
-      'title'=>video.title,
-      'id'=>video.id,
-      'youtube_id'=>video.youtube_id,
-      'channel_name'=>video.channel_name,
-      'channel_id'=>video.channel_id,
-      'total_seconds'=>video.total_seconds,
-      'published_at'=> favorite.video.published_at,
+      'title'=> video.title,
+      'id'=> video.id,
+      'youtube_id'=> video.youtube_id,
+      'channel_name'=> video.channel_name,
+      'channel_id'=> video.channel_id,
+      'total_seconds'=> video.total_seconds,
+      'published_at'=> video.published_at,
     }}
     respond_to do |format|
       format.html
