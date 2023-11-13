9037d6ce12794a621e99af0808ee4cf8895ed290
haseary <hase.ray.business@gmail.com>
2023-11-13 17:42:24 +0900

    added remove_duplicates to VideoCategory

--- app/models/video_category.rb
+++ app/models/video_category.rb
@@ -1,5 +1,16 @@
 class VideoCategory < ApplicationRecord
-    belongs_to :category
-    belongs_to :video
-    #validates :video_id, uniqueness: { scope: :category_id }
+  belongs_to :category
+  belongs_to :video
+  #validates :video_id, uniqueness: { scope: :category_id }
+  def self.remove_duplicates
+    duplicates = select(:video_id, :category_id)
+                 .group(:video_id, :category_id)
+                 .having('COUNT(*) > 1')
+    
+    # 重複しているレコードのうち、最初以外を削除
+    duplicates.each do |duplicate|
+      records_to_delete = where(video_id: duplicate.video_id, category_id: duplicate.category_id)
+      records_to_delete.offset(1).destroy_all
+    end
+  end
 end
